cd7c2c60be53612b980fd4b3fe981b22d0a2bba6
0871063 <c0871063@mylambton.ca>
2022-10-23 14:36:08 -0400

    Cancel Button and Error Checking

--- Malsha_C0871063_ContactApp/Malsha_C0871063_ContactApp/AddContactViewController.swift
+++ Malsha_C0871063_ContactApp/Malsha_C0871063_ContactApp/AddContactViewController.swift
@@ -9,11 +9,12 @@ import UIKit
 
 class AddContactViewController: UIViewController {
     
-    @IBOutlet weak var firstNameText: UITextField!    
+    @IBOutlet weak var firstNameText: UITextField!
     @IBOutlet weak var emailText: UITextField!
     @IBOutlet weak var phoneNumberText: UITextField!
     @IBOutlet weak var companyText: UITextField!
     @IBOutlet weak var lastNameText: UITextField!
+    @IBOutlet weak var addOrFindSegment: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +23,14 @@ class AddContactViewController: UIViewController {
     
 
     @IBAction func AddOrFindContact(_ sender: UISegmentedControl) {
+        switch addOrFindSegment.selectedSegmentIndex {
+        case 0:
+            break
+        case 1 :
+            break
+        default:
+            break
+        }
     }
     
     @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
@@ -29,9 +38,44 @@ class AddContactViewController: UIViewController {
     }
     
     @IBAction func saveBtnClicked(_ sender: Any) {
+        
+        if firstNameText.text == "" {
+            showAlert(title: "Error", actionTitle: "OK", message: "First name cannot be empty.", preferredStyle: .alert)
+        }else if lastNameText.text == "" {
+            showAlert(title: "Error", actionTitle: "OK", message: "Last name cannot be empty.", preferredStyle: .alert)
+        }else if companyText.text == "" {
+            showAlert(title: "Error", actionTitle: "OK", message: "Company cannot be empty.", preferredStyle: .alert)
+        }else if phoneNumberText.text == "" {
+            showAlert(title: "Error", actionTitle: "OK", message: "Phone number cannot be empty.", preferredStyle: .alert)
+        }else if emailText.text == "" {
+            showAlert(title: "Error", actionTitle: "OK", message: "Email cannot be empty.", preferredStyle: .alert)
+        }else{
+            
+        }
     }
     
     @IBAction func cancelBtnClicked(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "Are you sure?", message: nil, preferredStyle: .actionSheet)
+        let cancelAction = UIAlertAction(title: "No Way!", style: .cancel)
+        let sureAction = UIAlertAction(title: "Yes, I'm Sure!", style: .default, handler: {_ in
+            self.firstNameText.text = ""
+            self.lastNameText.text = ""
+            self.companyText.text = ""
+            self.phoneNumberText.text = ""
+            self.emailText.text = ""
+        })
+        alert.addAction(sureAction)
+        alert.addAction(cancelAction)
+        self.present(alert, animated: true)
+    }
+    
+    private func showAlert(title : String, actionTitle : String, message : String, preferredStyle : UIAlertController.Style){
+        
+        let alert = UIAlertController(title:title , message:message , preferredStyle: preferredStyle)
+        let action = UIAlertAction(title: actionTitle, style: .cancel)
+        alert.addAction(action)
+        present(alert, animated: true)
     }
     
 }
